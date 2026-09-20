3de527e431bb8b8f66a2f329aa4419aa529645c8
NEO Self Evolution <actions@users.noreply.github.com>
2026-09-20 04:53:00 +0000

    NEO autonomous bounded policy evolution

--- cloud_mcp.py
+++ cloud_mcp.py
@@ -19,7 +19,7 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.routing import Mount, Route
 
-VERSION = "0.45.0"
+VERSION = "0.45.1"
 MCP_REGISTRY = "https://registry.modelcontextprotocol.io"
 A2A_REGISTRY = "https://a2aregistry.org"
 RENDER_API_BASE = "https://api.render.com/v1"
