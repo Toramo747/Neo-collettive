378a307f896de1066f9846f958fff7c8c21936df
Toramo747 <gava.andrea@gmail.com>
2026-09-21 22:11:45 +0200

    Finalize NEO v0.68 retrieval hardening

--- cloud_mcp.py
+++ cloud_mcp.py
@@ -37,7 +37,7 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.routing import Mount, Route
 
-VERSION = "0.68.0"
+VERSION = "0.68.0"  # retrieval hardening
 MCP_REGISTRY = "https://registry.modelcontextprotocol.io"
 A2A_REGISTRY = "https://a2aregistry.org"
 RENDER_API_BASE = "https://api.render.com/v1"
