588a1997e53f41c32c10cf800742a98790a4c8a6
Toramo747 <gava.andrea@gmail.com>
2026-09-22 12:11:47 +0200

    Finalize v0.73 commercial routing with regression coverage

--- cloud_mcp.py
+++ cloud_mcp.py
@@ -51,7 +51,7 @@ from starlette.requests import Request
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.routing import Mount, Route
 
-VERSION = "0.73.0"  # commercial-source routing for PAID_DEMAND
+VERSION = "0.73.0"  # commercial-source routing for PAID_DEMAND (Remotive + Remote OK)
 MCP_REGISTRY = "https://registry.modelcontextprotocol.io"
 A2A_REGISTRY = "https://a2aregistry.org"
 RENDER_API_BASE = "https://api.render.com/v1"
